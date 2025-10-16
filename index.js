a86ac38f1735d2b1e437e2c46a282bc37c02f7aa
Emanuel Andersen <ea225mw@student.lnu.se>
2025-10-16 09:38:47 +0200

    Add countWords and countLetters to exported public methods.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import { PhraseCounter } from "./src/PhraseCounter.js"
 import { StringTransformer } from "./src/StringTransformer.js"
 import { WordLengthFinder } from "./src/WordLengthFinder.js"
 import { WordSorter } from "./src/WordSorter.js"
+import { WordAndLetterCounter } from "./src/WordAndLetterCounter.js"
 
 const mostFrequentLetterFinder = new MostFrequentLetterFinder
 const phraseCounter = new PhraseCounter
 const stringTransformer = new StringTransformer
 const wordLengthFinder = new WordLengthFinder
 const wordSorter = new WordSorter
+const wordAndLetterCounter = new WordAndLetterCounter
 
 export const stringWorks = {
   mostFrequentLetter: (string) => mostFrequentLetterFinder.mostFrequentLetter(string),
@@ -18,5 +20,7 @@ export const stringWorks = {
   findLongestWord: (string) => wordLengthFinder.findLongestWord(string),
   findShortestWord: (string) => wordLengthFinder.findShortestWord(string),
   sortWordsAscending: (string) => wordSorter.sortWordsAscending(string),
-  sortWordsDescending: string => wordSorter.sortWordsDescending(string)
+  sortWordsDescending: (string) => wordSorter.sortWordsDescending(string),
+  countWords: (string) => wordAndLetterCounter.countWords(string),
+  countLetters: (string) => wordAndLetterCounter.countLetters(string)
 }
--- tests/test-index.js
+++ tests/test-index.js
@@ -1,6 +1,6 @@
 import { testStr1, testStr2, testStr3, testStr4, testStr5, testStr6 } from "./testStrings.js"
 
-import { stringWorks } from "../src/index.js";
+import { stringWorks } from "../index.js";
 
 console.log(stringWorks.mostFrequentLetter(testStr1))
 console.log(stringWorks.mostFrequentLetterCS(testStr1))
@@ -8,3 +8,6 @@ console.log(stringWorks.countSpecifiedPhrase(testStr1, 'g'))
 console.log(stringWorks.makeFirstLetterCapital(testStr4))
 console.log(stringWorks.findLongestWord(testStr2))
 console.log(stringWorks.findShortestWord(testStr2))
+console.log(stringWorks.countWords(testStr2))
+console.log(stringWorks.countLetters(testStr1))
+
